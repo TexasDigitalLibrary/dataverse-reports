c093445edf20a409338053b1781923f097b4f03c
nwoodward <woodward.nicholas@gmail.com>
2021-03-26 15:11:57 -0500

    update version for release; simplified version text

--- setup.py
+++ setup.py
@@ -4,29 +4,11 @@ import re
 
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
-
-    raise RuntimeError("Unable to find version string.")
-
 install_requires = ['dataverse-client-python']
 
 setup(
     name = 'dataverse-reports',
-    version=find_version("lib", "__init__.py"),
+    version='1.1.1',
     url = 'https://www.tdl.org/',
     author = 'Nicholas Woodward',
     author_email = 'njw@austin.utexas.edu',
